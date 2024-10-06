cc1fc1ad326a98948740a3e2119626d7d914fda0
DragonCode <151834618+Whitedragon115@users.noreply.github.com>
2024-10-06 13:39:03 +0800

    auto update (Power By Whitedragon)

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the 'public' directory, and map index.html to the folder path
+app.use(express.static(path.join(__dirname, 'public'), {
+    extensions: ['html'],
+}));
+
+app.get('/:folder', (req, res) => {
+    const folderPath = path.join(__dirname, 'public', req.params.folder, 'index.html');
+    res.sendFile(folderPath, (err) => {
+        if (err) {
+            res.status(404).send('Page not found');
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
