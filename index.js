dee0f11b6369680d64b4126835a96b99babc2b79
DragonCode <151834618+Whitedragon115@users.noreply.github.com>
2024-10-08 14:51:21 +0800

    auto update (Power By Whitedragon)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 4000;
+const PORT = 4002;
 
 // Serve static files from the 'public' directory, and map index.html to the folder path
 app.use(express.static(path.join(__dirname, 'public'), {
